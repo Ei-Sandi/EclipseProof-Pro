1f773a3620607186a924ec883732b3456ae35a4d
Ei-Sandi <eisandiaung.esa@gmail.com>
2025-11-22 16:40:35 +0000

    Fix witness and index from contracts

--- contracts/src/witnesses.ts
+++ contracts/src/witnesses.ts
@@ -1,12 +1,20 @@
 import type { WitnessContext } from '@midnight-ntwrk/compact-runtime';
-import type { Ledger, Witnesses } from '../managed/ep-contract/contract/index.cjs';
+import type { Ledger, Witnesses as ContractWitnesses } from '../managed/ep-contract/contract/index.cjs';
 
 export type PayslipPrivateState = {
   idDOB: Uint8Array;
 };
 
-export const witnesses: Witnesses<PayslipPrivateState> = {
-  getIDDOB: ({ privateState }: WitnessContext<Ledger, PayslipPrivateState>) => {
+export function createPaySlipPrivateState(idDOB: Uint8Array): PayslipPrivateState {
+  return { idDOB };
+}
+
+export type PayslipWitnesses = ContractWitnesses<PayslipPrivateState>;
+
+export const witnesses: PayslipWitnesses = {
+  getIDDOB: (
+    { privateState }: WitnessContext<Ledger, PayslipPrivateState>,
+  ): [PayslipPrivateState, Uint8Array] => {
     return [privateState, privateState.idDOB];
   },
 };
